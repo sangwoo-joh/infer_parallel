d285cbd6a4e75fda6cc9f30c4c2cd5cb8601d322
Sangwoo Joh <lambdasnow@gmail.com>
2021-06-01 16:21:27 +0900

    Add comments

--- src/Config.mli
+++ src/Config.mli
@@ -9,12 +9,16 @@
 open! IStd
 
 val oom_threshold : int option ref
+(** Programmable threshold for out-of-memory. *)
 
 val progress_bar : [`Multiline | `Plain | `Quiet] ref
+(** The kind of progress bar. It is ok not to have multiline progress bar. *)
 
 val jobs : int ref
+(** The number of parallel workers. *)
 
 val keep_going : bool ref
+(** Whether or not to continue the full tasks even if some of the children had died. *)
 
 type verbose_level = Quiet | Moderate | Verbose
 
